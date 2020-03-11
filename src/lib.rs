d6916795d08c993316b66df360e9199ba1e5393a
David Pacheco <dap@oxidecomputer.com>
2020-03-10 19:16:57 -0700

    more simplifying comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 /*!
- * Hello
- *
  * * Hello
  *     * Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacinia
  *       ullamcorper lorem, non hendrerit enim convallis ut. Curabitur id sem
